a6b37a31225344d0f73fd1e58815733610fa344d
Jens Balvig <jens@balvig.com>
2018-04-08 17:48:45 +0900

    fix linebreaking for new font

--- test/linebreaker_test.rb
+++ test/linebreaker_test.rb
@@ -5,13 +5,19 @@ module LifeboxesApi
     def test_linebreaking_1
       result = Linebreaker.new("The simple fun of blowing easter eggs without having to worry about salmonella").run
 
-      assert_equal "The simple fun of blowing easter\neggs without having to worry about\nsalmonella", result
+      assert_equal "The simple fun of blowing easter\neggs without having to worry\nabout salmonella", result
     end
 
     def test_linebreaking_2
       result = Linebreaker.new("Was able to spend a relaxing Sunday").run
 
       assert_equal "Was able to spend a relaxing\nSunday", result
+    end
+
+    def test_linebreaking_3
+      result = Linebreaker.new("Getting a spontaneous foot massage after a long day").run
+
+      assert_equal "Getting a spontaneous foot\nmassage after a long day", result
     end
   end
 end
