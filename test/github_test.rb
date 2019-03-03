de46f91741500bc30e756b03d0b829e149b96cc0
Jens Balvig <jens@balvig.com>
2019-03-03 17:26:26 +0900

    remove junk

--- test/github_test.rb
+++ test/github_test.rb
@@ -6,15 +6,5 @@ module LifeboxesApi
       github = Github.new
       assert_equal ["ci: failure"], github.statuses
     end
-
-    #def test_current_temperature
-      #weather = Weather.new
-      #assert_equal 6, weather.current_temperature
-    #end
-
-    #def test_todays_lowest_temperature
-      #weather = Weather.new
-      #assert_equal 5, weather.todays_lowest_temperature
-    #end
   end
 end
